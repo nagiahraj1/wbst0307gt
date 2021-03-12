c64761481a23fe1e8b75ae41d8ab8c936eea8b3b
nagiahraj1 <44737630+nagiahraj1@users.noreply.github.com>
2021-03-12 16:35:49 -0500

    add form2

---
+++ wbst0307/WebForm2.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wbst0307
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Text = DateTime.Now.ToLongTimeString();
+
+        }
+    }
+}
